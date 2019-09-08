ef01fb0ab6c5d030340f34106c276fc0045c3884
Oka Prinarjaya <okaprinarjaya@gmail.com>
2019-09-09 03:05:12 +0700

    Update 09 Sept 2019 03:05

--- src/oprex_order_manager.erl
+++ src/oprex_order_manager.erl
@@ -1,11 +1,10 @@
 -module(oprex_order_manager).
 -behaviour(gen_server).
 
--export([start_link/0, create_workers/1, initiate_order/1, next_order/5, initiate_worker_data/1]).
+-export([start_link/0, create_workers/2, initiate_order/1, next_order/5]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -define(SRV, oprex_order_manager_srv).
--define(POOL_ARGS(WorkerGroupName), [{name, {local, WorkerGroupName}}, {worker_module, oprex_worker}, {size, 100}, {max_overflow, 0}]).
 -define(WORKER_ARGS, [{dbhost, "localhost"}, {dbname, "dpt_in_da_house"}, {dbuser, "mamp"}, {dbpasswd, "12qwaszx@321"}]).
 -define(TOTAL_ROWS, 1000000).
 -define(DISTRIBUTE_ROWS_PER_PAGE, 10000).
@@ -15,14 +14,16 @@
 % -define(QUERY_STR, <<"SELECT id, nama, status_dpt FROM dpt_pemilihbali LIMIT ?, ?">>).
 -define(QUERY_STR, <<"SELECT 1 AS id, CONCAT(transaksi_id, ' ', kuesioner_id, ' ', pilihan_jawaban_id, ' ', pilihan_lain) AS nama, 3 AS status_dpt FROM data_masuk ORDER BY transaksi_id ASC LIMIT ?, ?">>).
 
--record(state, {procs = [], orders = []}).
+-record(state, {procs_pids = [], procs_refs = [], orders = []}).
 
 start_link() ->
   gen_server:start_link({local, ?SRV}, ?MODULE, [], []).
 
 init(_Args) ->
-  gen_server:cast(self(), {start_oprex_worker_supervisor, {test_worker_server, start_link, []}}),
-  {ok, #state{procs = [], orders = []}}.
+  MFA = {test_worker_server, start_link, []},
+  gen_server:cast(self(), {start_oprex_worker_supervisor, MFA}),
+
+  {ok, #state{procs_pids = [], procs_refs = [], orders = []}}.
 
 handle_cast({start_oprex_worker_supervisor, {M, F, A}}, State) ->
   ChildSpecs = #{
@@ -33,11 +34,10 @@ handle_cast({start_oprex_worker_supervisor, {M, F, A}}, State) ->
     type => supervisor,
     modules => [oprex_worker_supervisor]
   },
-  {ok, Pid} = supervisor:start_child(
+  {ok, _Pid} = supervisor:start_child(
     oprex_supervisor,
     ChildSpecs
   ),
-  link(Pid),
 
   {noreply, State}.
 
@@ -67,18 +67,25 @@ handle_cast({start_oprex_worker_supervisor, {M, F, A}}, State) ->
 
 %   {noreply, State}.
 
-handle_call({create_workers, _Args}, _From, #state{procs = Processes} = State) ->
-  {ok, Pid} = supervisor:start_child(oprex_worker_supervisor, [{wrk1args1, wrk1args2, wrk1args3}]),
-  Ref = erlang:monitor(process, Pid),
-  io:format("Worker added: ~p~n", [Ref]),
+handle_call({create_workers, {TotalRows, RowsPerPage}}, _From, #state{procs_pids = PidsCurrent, procs_refs = RefsCurrent} = State) ->
+  TotalPages = ceil(TotalRows / RowsPerPage),
+  Pages = lists:seq(0, (TotalPages - 1)),
+  ProcessesNew = [worker_starter(Page, RowsPerPage) || Page <- Pages],
+  {PidsNew, RefsNew} = pids_refs(ProcessesNew),
 
-  {reply, okdokie, State#state{procs = [Pid | Processes]}}.
+  {reply, ok, State#state{procs_pids = lists:append([PidsNew, PidsCurrent]), procs_refs = lists:append([RefsNew, RefsCurrent])}}.
 
-handle_info(procsnumbers, #state{procs = Processes} = State) ->
-  io:format("Processes numbers: ~p~n", [length(Processes)]),
-  io:format("Processes: ~p~n", [Processes]),
+handle_info({'DOWN', Ref, process, Pid, _}, #state{procs_refs = Refs, procs_pids = Processes} = State) ->
+  case lists:member(Ref, Refs) of
+    true ->
+      NewRefs = lists:delete(Ref, Refs),
+      NewPids = lists:delete(Pid, Processes),
 
-  {noreply, State};
+      {noreply, State#state{procs_refs = NewRefs, procs_pids = NewPids}};
+
+    false ->
+      {noreply, State}
+  end;
 
 handle_info(_Info, State) ->
   {noreply, State}.
@@ -89,47 +96,27 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
-create_workers({M, F, A}) ->
-  Resp = gen_server:call(?SRV, {create_workers, {M, F, A}}),
-  io:format("Resp = ~p~n", [Resp]).
+create_workers(TotalRows, RowsPerPage) ->
+  gen_server:call(?SRV, {create_workers, {TotalRows, RowsPerPage}}).
 
-initiate_worker_data(WorkerGroupName) ->
-  TotalPages = ceil(?TOTAL_ROWS / ?DISTRIBUTE_ROWS_PER_PAGE),
-  Pages = lists:seq(0, TotalPages - 1),
-  gen_server:cast(?SRV, {initiate_worker_data, WorkerGroupName, Pages}).
+worker_starter(Page, Limit) ->
+  Offset = Limit * Page,
+  {ok, Pid} = supervisor:start_child(oprex_worker_supervisor, [Offset, Limit]),
+  Ref = erlang:monitor(process, Pid),
+
+  {Pid, Ref}.
+
+pids_refs(ListOfPidsRefs) ->
+  pids_refs(ListOfPidsRefs, [], []).
+pids_refs([], Pids, Refs) ->
+  {Pids, Refs};
+pids_refs(ListOfPidsRefs, Pids, Refs) ->
+  [Head | Tail] = ListOfPidsRefs,
+  {Pid, Ref} = Head,
+  pids_refs(Tail, [Pid | Pids], [Ref | Refs]).
 
 initiate_order(WorkerGroupName) ->
-  io:format("Start at: ~p~n", [calendar:local_time()]),
   gen_server:cast(?SRV, {initiate_order, WorkerGroupName}).
 
 next_order(WorkerPid, Ref, TheOrderPage, TotalTheOrders, TotalTheOrdersReceived) ->
   gen_server:cast(?SRV, {next_order, WorkerPid, Ref, TheOrderPage, TotalTheOrders, TotalTheOrdersReceived}).
-
-distribute_data(_, _WorkerGroupName, []) ->
-  ok;
-
-distribute_data(DbConn, WorkerGroupName, Pages) ->
-  [Page | TailPages] = Pages,
-  spawn(fun() ->
-    poolboy:transaction(
-      WorkerGroupName,
-      fun(Worker) ->
-        gen_server:cast(Worker, {distribute_data, DbConn, Page})
-      end
-    )
-  end),
-  distribute_data(DbConn, WorkerGroupName, TailPages).
-
-distribute_initial_order(101, _WorkerGroupName, _TotalTheOrders, _InitialTheOrders) ->
-  ok;
-
-distribute_initial_order(Incr, WorkerGroupName, TotalTheOrders, InitialTheOrders) ->
-  spawn(fun() ->
-    poolboy:transaction(
-      WorkerGroupName,
-      fun(Worker) ->
-        gen_server:cast(Worker, {initiate_order, make_ref(), TotalTheOrders, InitialTheOrders})
-      end
-    )
-  end),
-  distribute_initial_order(Incr + 1, WorkerGroupName, TotalTheOrders, InitialTheOrders).
