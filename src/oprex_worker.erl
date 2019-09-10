a80ab53a2b6309a7a2381e0528572e3784a54d41
Oka Prinarjaya <okaprinarjaya@gmail.com>
2019-09-11 01:36:36 +0700

    Updates - 11 Sept 2019 01:36

--- src/oprex_worker.erl
+++ src/oprex_worker.erl
@@ -1,7 +1,7 @@
 -module(oprex_worker).
 -behaviour(gen_server).
 
--export([start_link/1]).
+-export([start_link/2]).
 -export([
   init/1,
   handle_call/3,
@@ -11,126 +11,64 @@
   code_change/3
 ]).
 
--define(DISTRIBUTE_ROWS_PER_PAGE, 10000).
 % -define(QUERY_STR, <<"SELECT id, nama, status_dpt FROM dpt_pemilihbali LIMIT ?, ?">>).
+
 -define(QUERY_STR, <<"SELECT 1 AS id, CONCAT(transaksi_id, ' ', kuesioner_id, ' ', pilihan_jawaban_id, ' ', pilihan_lain) AS nama, 3 AS status_dpt FROM data_masuk ORDER BY transaksi_id ASC LIMIT ?, ?">>).
 
--record(state, {
-  rows, rows_received,
-  the_order_page, doubled_data_found
-}).
+-record(state, {rows = [], total_orders_received = 0, orders_paging = 0}).
 
-start_link(Args) ->
-  gen_server:start_link(?MODULE, Args, []).
+start_link(Offset, Limit) ->
+  gen_server:start_link(?MODULE, [Offset, Limit], []).
 
-init(_Args) ->
-  {ok, #state{
-    the_order_page = 0, rows = [],
-    rows_received = 0, doubled_data_found = []
-  }}.
+init([Offset, Limit]) ->
+  {ok, DbCredentials} = application:get_env(dist_procs_je_asane, dbcredentials),
+  DbHost = proplists:get_value(dbhost, DbCredentials),
+  DbName = proplists:get_value(dbname, DbCredentials),
+  DbUser = proplists:get_value(dbuser, DbCredentials),
+  DbPasswd = proplists:get_value(dbpasswd, DbCredentials),
 
-handle_call(get_state, _From, State) ->
-  #state{
-    rows=Rows,
-    rows_received=RowsReceived,
-    the_order_page=TheOrderPage
-  } = State,
-  Reply = {ok, [
-    {rows, Rows}, {rows_received, RowsReceived},
-    {the_order_page, TheOrderPage}
-  ]},
+  {ok, DbConn} = mysql:start_link([
+    {host, DbHost},
+    {port, 8889},
+    {user, DbUser},
+    {password, DbPasswd},
+    {database, DbName}
+  ]),
 
-  {reply, Reply, State};
+  {ok, _, Rows} = mysql:query(DbConn, ?QUERY_STR, [Offset, Limit]),
+  ok = mysql:stop(DbConn),
 
-handle_call(_Request, _From, State) ->
-  {reply, ok, State}.
+  {ok, #state{rows = Rows}}.
 
-handle_cast({distribute_data, DbConn, Page}, State) ->
-  OffsetStart = ?DISTRIBUTE_ROWS_PER_PAGE * Page,
-  {ok, _, Rows} = mysql:query(DbConn, ?QUERY_STR, [OffsetStart, ?DISTRIBUTE_ROWS_PER_PAGE]),
+handle_cast({initial_orders, Ref, OrdersSubmit}, #state{rows = Rows, orders_paging = Page} = State) ->
+  TotalOrdersReceivedNextLen = length(OrdersSubmit),
+  NextPage = Page + 1,
 
-  {noreply, State#state{
-    rows = Rows
-  }};
+  compare(OrdersSubmit, Rows, self()),
 
-handle_cast({initiate_order, Ref, TotalTheOrders, InitialTheOrders}, State) ->
-  #state{the_order_page=TheOrderPage, rows = WorkerDataDPT} = State,
-  RowsReceived = length(InitialTheOrders),
+  oprex_order_manager:next_orders(self(), Ref, NextPage, TotalOrdersReceivedNextLen),
 
-  lists:foreach(
-    fun(ElemTheOrder) ->
-      [_IdTheOrder, NamaTheOrder, _] = ElemTheOrder,
-      lists:foreach(
-        fun(ElemGalaxy) ->
-          [_IdGalaxy, NamaGalaxy, _] = ElemGalaxy,
+  {noreply, State#state{orders_paging = NextPage, total_orders_received = TotalOrdersReceivedNextLen}};
 
-          if NamaTheOrder =/= <<>> andalso NamaGalaxy =/= <<>> andalso NamaTheOrder =/= NamaGalaxy  ->
-            CompareLen = binary:longest_common_prefix([NamaTheOrder, NamaGalaxy]),
-            BLen = byte_size(NamaGalaxy),
-            Similarity = (CompareLen / BLen) * 100,
+handle_cast({next_orders, Ref, NextOrdersSubmit}, State) ->
+  #state{rows = Rows, total_orders_received = TotalOrdersReceivedLen, orders_paging = Page} = State,
+  TotalOrdersReceivedNextLen = TotalOrdersReceivedLen + length(NextOrdersSubmit),
+  NextPage = Page + 1,
 
-            if Similarity > 90 ->
-              io:format("Double data detected! - (~p) is similar with (~p) found at: ~p~n", [NamaTheOrder, NamaGalaxy, self()]);
-              true -> ok
-            end;
+  compare(NextOrdersSubmit, Rows, self()),
 
-            true -> ok
-          end
+  oprex_order_manager:next_orders(self(), Ref, NextPage, TotalOrdersReceivedNextLen),
 
-        end,
-        WorkerDataDPT
-      )
-    end,
-    InitialTheOrders
-  ),
-
-  order_manager:next_order(self(), Ref, TheOrderPage + 1, TotalTheOrders, RowsReceived),
-
-  {noreply, State#state{the_order_page = TheOrderPage + 1, rows_received = RowsReceived}};
-
-handle_cast({next_order, Ref, TotalTheOrders, NextTheOrders}, State) ->
-  #state{
-    the_order_page = TheOrderPage, rows = WorkerDataDPT,
-    rows_received = RowsReceived
-  } = State,
-  RowsReceivedNext = RowsReceived + length(NextTheOrders),
-
-  lists:foreach(
-    fun(ElemTheOrder) ->
-      [_IdTheOrder, NamaTheOrder, _] = ElemTheOrder,
-      lists:foreach(
-        fun(ElemGalaxy) ->
-          [_IdGalaxy, NamaGalaxy, _] = ElemGalaxy,
-
-          if NamaTheOrder =/= NamaGalaxy andalso NamaTheOrder =/= <<>> andalso NamaGalaxy =/= <<>>  ->
-            CompareLen = binary:longest_common_prefix([NamaTheOrder, NamaGalaxy]),
-            BLen = byte_size(NamaGalaxy),
-            Similarity = (CompareLen / BLen) * 100,
-
-            if Similarity > 90 ->
-              io:format("Double data detected! - (~p) is similar with (~p) found at: ~p~n", [NamaTheOrder, NamaGalaxy, self()]);
-              true -> ok
-            end;
-
-            true -> ok
-          end
-
-        end,
-        WorkerDataDPT
-      )
-    end,
-    NextTheOrders
-  ),
-
-  order_manager:next_order(self(), Ref, TheOrderPage + 1, TotalTheOrders, RowsReceivedNext),
-
-  {noreply, State#state{the_order_page = TheOrderPage + 1, rows_received = RowsReceivedNext}};
+  {noreply, State#state{orders_paging = NextPage, total_orders_received = TotalOrdersReceivedNextLen}};
 
 handle_cast(reset_state, State) ->
-  {noreply, State#state{the_order_page = 0, rows_received = 0}};
+  {noreply, State#state{total_orders_received = 0, orders_paging = 0}};
 
 handle_cast(_Msg, State) ->
   {noreply, State}.
+
+handle_call(_Request, _From, State) ->
+  {reply, ok, State}.
 
 handle_info(_Info, State) ->
   {noreply, State}.
@@ -140,3 +78,33 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
+
+compare(Orders, Rows, Pid) ->
+  lists:foreach(
+    fun(Order) ->
+      [_, OrderNama, _] = Order,
+      lists:foreach(
+        fun(Row) ->
+          [_, RowNama, _] = Row,
+
+          if
+            OrderNama =/= <<>> andalso RowNama =/= <<>> andalso OrderNama =/= RowNama  ->
+              CompareLen = binary:longest_common_prefix([OrderNama, RowNama]),
+              BLen = byte_size(RowNama),
+              Similarity = (CompareLen / BLen) * 100,
+
+              if
+                Similarity > 90 ->
+                  io:format("Double data detected! - (~p) is similar with (~p) found at: ~p~n", [OrderNama, RowNama, Pid]);
+                true -> ok
+              end;
+
+            true -> ok
+          end
+
+        end,
+        Rows
+      )
+    end,
+    Orders
+  ).
